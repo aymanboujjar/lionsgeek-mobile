2242151a58f2acdbdf7601de2ac74a10c4cfc77e
ayman boujjar <aymenboujjar12@gmail.com>
2026-01-27 10:52:03 +0100

    feature training show

---
+++ app/(tabs)/training/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router';
+
+export default function TrainingStack() {
+  return (
+    <Stack screenOptions={{ headerShown: false, gestureEnabled: true }}>
+      {/* List view */}
+      <Stack.Screen name="index" />
+      {/* Detail view */}
+      <Stack.Screen 
+        name="[id]" 
+        options={{
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          animation: 'slide_from_right',
+        }}
+      />
+    </Stack>
+  );
+}
